83ec700f1bdee7c9b5c9171bb8c894e8bec5b834
Phillipe_Rangel <ph.rangel@usp.br>
2018-11-11 04:54:18 -0200

    Add search enterprises projects

--- server.py
+++ server.py
@@ -7,14 +7,21 @@ import requests
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/pius", methods=['GET'])
-def get_all_projects():
+@app.route("/api/teachers_projects", methods=['GET'])
+def get_all_teacher_projects():
     #função que retorna a lista de projetos
-    with open('projects_database.txt', 'r') as project_database:
+    with open('teachers_projects_database.txt', 'r') as project_database:
         project_list = project_database.read()
     return json.dumps(project_list)
 
-@app.route("/cadastros", methods=['POST'])
+@app.route("/api/enterprises_projects", methods=['GET'])
+def get_all_enterprises_projects():
+    #função que retorna a lista de projetos
+    with open('enterprises_projects_database.txt', 'r') as project_database:
+        project_list = project_database.read()
+    return json.dumps(project_list)
+
+@app.route("/api/cadastros", methods=['POST'])
 def update_database():
     new_project = request.get_json()
     with open('projects_database.txt', 'r') as project_database:
